03a54304d8201738e8452129bb3735e629f7cb96
Vasyl <deresh14@gmail.com>
2026-01-01 18:38:17 +0100

    Added dropdown menu when hovering category btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./AppComponents/navbar/navbar";
 import Home from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/Login";
